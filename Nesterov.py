883160e38578a53a5d8b2d1302bbf8356ff602b9
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-11 17:21:33 -0500

    Create Nesterov.py

---
+++ Nesterov.py
@@ -0,0 +1,25 @@
+import GradientDescent as gd
+
+def momentum(velocity, beta, gradient, rate):
+	return beta * velocity + gradient * rate
+
+if __name__ == '__main__':
+	def test(x):
+		return x ** 2 + 7
+	
+	velocity = 0
+	beta = 0.9
+	dx = 0.001
+	a = 100
+	rate = 0.1
+
+	for t in range(1000):
+		print(a, test(a), velocity)
+
+		gradient = gd.gradientDescent(test, [a - velocity], [0], dx)
+		velocity = momentum(velocity, beta, gradient, rate)
+		a -= rate * velocity
+		
+		f = open('results.txt', 'a')
+		f.write(f'\n{test(a)}')
+		f.close()
