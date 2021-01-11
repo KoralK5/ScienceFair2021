6cee960c393a872804b86ae480b43de877a9b40e
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-10 23:38:42 -0500

    Update Momentum.py

--- Momentum.py
+++ Momentum.py
@@ -1,28 +1,19 @@
-import numpy as np
-import sympy as sym
+import GradientDescent as gd
 
-def test(x, a):
-	return x ** 2 - a * x
-
-def derivative(x, a):
-	return sym.diff(x ** 2 - a * x)
-
-def momentum(predw, beta, rate, a):
-	global weight
-
-	dw = derivative(weight, a)
-	
-	predw = beta * predw + (1-beta)*dw
-
-	return weight - rate * predw
+def momentum(velocity, beta, func, args, position, dx):
+	return beta * velocity + gd.gradientDescent(func, args, position, dx)
 
 if __name__ == '__main__':
-	weight = 1000
-	a = 2000
-	rate = 0.1
+	def test(x):
+		return x ** 2 + 7
+	
+	velocity = 0
 	beta = 0.9
-	predw = 0
+	dx = 0.001
+	a = 10
+	rate = 0.1
 
-	for t in range(100):
-		print(weight, test(weight, a))
-		weight = momentum(weight, beta, rate, a)
+	for t in range(100000):
+		print(a, test(a), velocity)
+		velocity = momentum(velocity, beta, test, [a], [0], dx)
+		a -= rate * velocity
