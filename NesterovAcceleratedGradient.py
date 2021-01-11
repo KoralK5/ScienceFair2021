2e8212efe3437e398caa558c89ec395cee4c901e
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-11 11:53:02 -0500

    Create NesterovAcceleratedGradient.py

---
+++ NesterovAcceleratedGradient.py
@@ -0,0 +1,22 @@
+import GradientDescent as gd
+
+def momentum(velocity, beta, func, args, position, dx):
+	return beta * velocity + gd.gradientDescent(func, args, position, dx)
+
+if __name__ == '__main__':
+	def test(x):
+		return x ** 2 + 7
+	
+	velocity = 0
+	beta = 0.9
+	dx = 0.001
+	a = 10
+	rate = 0.1
+
+	for t in range(10000):
+		print(a, test(a), velocity)
+		velocity = momentum(velocity, beta, test, [a-velocity], [0], dx)
+		a -= rate * velocity
+		f = open('main.pi', 'a')
+		f.write(f'\n{test(a)}')
+		f.close()
