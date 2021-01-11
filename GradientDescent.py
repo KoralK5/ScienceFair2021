7cd708399bc8cac91738bed2845a81d5295c28fa
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-10 23:38:23 -0500

    Update GradientDescent.py

--- GradientDescent.py
+++ GradientDescent.py
@@ -6,31 +6,20 @@ def change(l, i, x):
 	else:
 		l[i[0]] += x
 
-def cost(func, args, targets):
-	outputs = np.array(func(*args))
-	targets = np.array(targets)
-	error = (outputs - targets) ** 2
-	return np.sum(error)
+def gradientDecent(func, args, pos, dx):
+	nArgs = args[:]
+	change(nArgs, pos, dx)
+	return func(*nArgs) - func(*args)
 
-def gradientDescent(func, args, targets, h, position):
-	#print(args)
-	cost1 = cost(func, args, targets)
-	newArgs = list(args)[:]
-	change(newArgs, position, h)
-	newArgs = tuple(newArgs)
-	#print(newArgs)
-	cost2 = cost(func, newArgs, targets)
-	#print(cost1, cost2)
-	return (cost1 - cost2) / h
+if __name__ == '__main__':
+	def test(x):
+		return x**2 + 5
 
-def test(x, a):
-	return x ** 2 + a * x
+	rate = 0.1
+	dx = 0.01
+	a = 10
 
-a = 0.001
-t = 150
-h = 0.1
-w = 2
-
-for x in range(100):
-	print(w, test(10, w))
-	w += gradientDescent(test, (10, w), 150, h, [1]) * a
+	for t in range(10000):
+		adjust = gradientDecent(test, [a], [0], dx)
+		print(adjust, a, test(a))
+		a -= adjust * rate
