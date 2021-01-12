c4f81a349b8dce919a536ef74775390ea3420e15
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-11 23:17:46 -0500

    Update Nesterov.py

--- Nesterov.py
+++ Nesterov.py
@@ -4,17 +4,18 @@ def momentum(velocity, beta, gradient, rate):
 	return beta * velocity + gradient * rate
 
 def demo(
+	test,
 	tolerance = 0.001, 
 	velocity = 0,
-	beta = 0.9, 
+	beta = 0.9,
 	dx = 0.001, 
 	x = 100, 
 	rate = 0.1
 	):
 
-	def test(x):
-		return x ** 2 + 7
-	
+	f = open('Nesterov.txt', 'a')
+	f.write('Nesterov'); f.close()
+
 	t = 0
 	while test(x) > 7 + tolerance:
 		print(f'\nx: {x}\nf(x): {test(x)}\nvelocity: {velocity}')
@@ -24,11 +25,14 @@ def demo(
 		x -= rate * velocity
 		t += 1
 		
-		f = open('results.txt', 'a')
-		f.write(f'\n{test(x)}')
-		f.close()
-	print(f'\n\nLocal Minimum: {test(x)}')
+		f = open('Nesterov.txt', 'a')
+		f.write(f'\n{test(x)}'); f.close()
+	
+	print('\n\nNESTEROV')
+	print(f'Local Minimum: {test(x)}')
 	print(f'Iterations: {t}')
+
+	return test(x), t
 
 if __name__ == '__main__':
 	demo()
