63680484726b12753a0a310aca6ca2c1ba313548
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-11 21:12:09 -0500

    Update Nesterov.py

--- Nesterov.py
+++ Nesterov.py
@@ -3,23 +3,32 @@ import GradientDescent as gd
 def momentum(velocity, beta, gradient, rate):
 	return beta * velocity + gradient * rate
 
-if __name__ == '__main__':
+def demo(
+	tolerance = 0.001, 
+	velocity = 0,
+	beta = 0.9, 
+	dx = 0.001, 
+	x = 100, 
+	rate = 0.1
+	):
+
 	def test(x):
 		return x ** 2 + 7
 	
-	velocity = 0
-	beta = 0.9
-	dx = 0.001
-	a = 100
-	rate = 0.1
-
-	for t in range(1000):
-		print(a, test(a), velocity)
-
-		gradient = gd.gradientDescent(test, [a - velocity], [0], dx)
+	t = 0
+	while test(x) > 7 + tolerance:
+		print(f'\nx: {x}\nf(x): {test(x)}\nvelocity: {velocity}')
+		
+		gradient = gd.gradientDescent(test, [x - velocity], [0], dx)
 		velocity = momentum(velocity, beta, gradient, rate)
-		a -= rate * velocity
+		x -= rate * velocity
+		t += 1
 		
 		f = open('results.txt', 'a')
-		f.write(f'\n{test(a)}')
+		f.write(f'\n{test(x)}')
 		f.close()
+	print(f'\n\nLocal Minimum: {test(x)}')
+	print(f'Iterations: {t}')
+
+if __name__ == '__main__':
+	demo()
