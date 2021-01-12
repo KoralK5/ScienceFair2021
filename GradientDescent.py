5df3d0d3e426bb47bfeb4c6e907ed680061dded2
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-11 20:07:08 -0500

    Update GradientDescent.py

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,25 +1,26 @@
-import numpy as np
-
 def change(l, i, x):
 	if len(i) > 1:
-	    change(l[i[0]], i[1:len(i)], x)
+		change(l[i[0]], i[1:len(i)], x)
 	else:
 		l[i[0]] += x
 
-def gradientDecent(func, args, pos, dx):
+def gradientDescent(func, args, pos, dx):
 	nArgs = args[:]
 	change(nArgs, pos, dx)
-	return func(*nArgs) - func(*args)
+	return (func(*nArgs) - func(*args)) / dx
 
 if __name__ == '__main__':
 	def test(x):
-		return x**2 + 5
-
+		return (x)**2 + 7
 	rate = 0.1
-	dx = 0.01
-	a = 10
+	dx = 0.001
+	a = 100
 
-	for t in range(10000):
-		adjust = gradientDecent(test, [a], [0], dx)
-		print(adjust, a, test(a))
+	for t in range(1000):
+		adjust = gradientDescent(test, [a], [0], dx)
 		a -= adjust * rate
+		print(a, test(a), adjust)
+
+		f = open('results.txt', 'a')
+		f.write(f'\n{test(a)}')
+		f.close()
