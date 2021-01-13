2ebe02b472be3dc6ca7d006813a3d195d4e1b04a
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-13 12:03:23 -0500

    Update NADAM.py

--- NADAM.py
+++ NADAM.py
@@ -5,28 +5,30 @@ def momentum(velocity, beta, gradient, rate):
 
 def demo(
 	test,
-	tolerance = 0.001, 
-	velocity = 0, 
-	beta = 0.9, 
-	dx = 0.001, 
-	x = 100, 
-	rate = 0.1
+	tolerance = 0.01,
+	x = 100,
+	dx = 0.001,
+	rate = 0.1,
+	vertex = 0,
+	beta = 0.9
 	):
 	
 	f = open('NADAM.txt', 'a')
 	f.write('NADAM'); f.close()
 
+	velocity = 1
 	t = 0
-	while test(x) > 7 + tolerance:
+	while test(x) > vertex + tolerance or test(x) < vertex - tolerance:
 		print(f'\nx: {x}\nf(x): {test(x)}\nvelocity: {velocity}')
 
 		gradient = gd.gradientDescent(test, [x], [0], dx)
 		velocity = momentum(velocity, beta, gradient, rate)
 		x -= beta * velocity + rate * gradient
-		t += 1
 
 		f = open('NADAM.txt', 'a')
 		f.write(f'\n{test(x)}'); f.close()
+
+		t += 1
 	
 	print('\n\nNADAM')
 	print(f'Local Minimum: {test(x)}')
