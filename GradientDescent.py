1cff36e3e7d9b97b08fe6414137287a1c32ee12b
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-13 12:02:46 -0500

    Update GradientDescent.py

--- GradientDescent.py
+++ GradientDescent.py
@@ -7,30 +7,32 @@ def change(l, i, x):
 def gradientDescent(func, args, pos, dx):
 	nArgs = args[:]
 	change(nArgs, pos, dx)
-	return (func(*nArgs) - func(*args)) * dx
+	return (func(*nArgs) - func(*args)) / dx
 
 def demo(
 	test,
-	tolerance = 0.001,
-	adjust = 0, 
-	rate = 0.1, 
+	tolerance = 0.01,
+	x = 100,
 	dx = 0.001, 
-	x = 100
+	rate = 0.1,
+	vertex = 0
 	):
 
 	f = open('GradientDescent.txt', 'a')
 	f.write('Gradient Descent'); f.close()
 
+	adjust = 0
 	t = 0
-	while test(x) > 7 + tolerance:
+	while test(x) > vertex + tolerance or test(x) < vertex - tolerance:
 		print(f'\nx: {x}\nf(x): {test(x)}\najustment: {adjust}')
 
 		adjust = gradientDescent(test, [x], [0], dx)
 		x -= adjust * rate
-		t += 1
 		
 		f = open('GradientDescent.txt', 'a')
 		f.write(f'\n{test(x)}'); f.close()
+		
+		t += 1
 
 	print('\n\nGRADIENT DESCENT')
 	print(f'Local Minimum: {test(x)}')
