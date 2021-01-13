c685fd9c98597f318a6c88144feab1df96c113c3
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-13 12:37:13 -0500

    Update Nesterov.py

--- Nesterov.py
+++ Nesterov.py
@@ -1,32 +1,34 @@
 import GradientDescent as gd
 
 def momentum(velocity, beta, gradient, rate):
-	return beta * velocity + gradient * rate
+	return (beta * velocity + gradient) * rate
 
 def demo(
 	test,
-	tolerance = 0.001, 
-	velocity = 0,
-	beta = 0.9,
-	dx = 0.001, 
-	x = 100, 
-	rate = 0.1
+	tolerance = 0.01,
+	x = 100,
+	dx = 0.001,
+	rate = 0.1,
+	vertex = 0,
+	beta = 0.9
 	):
 
 	f = open('Nesterov.txt', 'a')
 	f.write('Nesterov'); f.close()
 
+	velocity = 0
 	t = 0
-	while test(x) > 7 + tolerance:
+	while test(x) > vertex + tolerance or test(x) < vertex - tolerance:
 		print(f'\nx: {x}\nf(x): {test(x)}\nvelocity: {velocity}')
 		
 		gradient = gd.gradientDescent(test, [x - velocity], [0], dx)
 		velocity = momentum(velocity, beta, gradient, rate)
-		x -= rate * velocity
-		t += 1
+		x -= velocity
 		
 		f = open('Nesterov.txt', 'a')
 		f.write(f'\n{test(x)}'); f.close()
+
+		t += 1
 	
 	print('\n\nNESTEROV')
 	print(f'Local Minimum: {test(x)}')
