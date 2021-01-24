29462509d4f89e90a552151eaed9724531111a67
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-24 18:31:03 -0500

    Update GradientDescent.py

--- NeuralNetwork/GradientDescent.py
+++ NeuralNetwork/GradientDescent.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import NeuralNetwork as nn
 
 def change(l, i, x):
 	if len(i) > 1:
@@ -10,3 +11,11 @@ def gradientDescent(func, args, pos, dx):
 	nArgs = deepcopy(args)
 	change(nArgs[2], pos, dx)
 	return (func(*nArgs) - func(*args)) / dx
+
+def optimize(inputs, weights, outputs, iterations, rate):
+	newWeights = deepcopy(weights)
+	for row in range(len(newWeights)):
+		for column in range(len(newWeights[row])):
+			for n in range(iterations):
+				newWeights[row][column] -= gradientDescent(nn.cost, [deepcopy(inputs), deepcopy(outputs), deepcopy(newWeights)], [row, column], 0.001) * rate
+	return newWeights
