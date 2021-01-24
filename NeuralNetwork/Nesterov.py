954c8b19a6a644956d5ba9b5fd0e1104a0279f04
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-24 18:31:44 -0500

    Create Nesterov.py

---
+++ NeuralNetwork/Nesterov.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+import GradientDescent as gd
+import NeuralNetwork as nn
+
+def momentum(velocity, beta, gradient, rate):
+	return beta * velocity + gradient * rate
+
+def optimize(inputs, weights, outputs, iterations, rate, beta):
+	newWeights = deepcopy(weights)
+	velocity = 0
+	for row in range(len(newWeights)):
+		for column in range(len(newWeights[row])):
+			for n in range(iterations):
+				nudgedWeights = deepcopy(newWeights)
+				nudgedWeights[row][column] -= velocity
+
+				gradient = gd.gradientDescent(nn.cost, [deepcopy(inputs), deepcopy(outputs), deepcopy(nudgedWeights)], [row, column], 0.001) * rate
+				velocity = momentum(velocity, beta, gradient, rate)
+				
+				newWeights[row][column] -= velocity
+	return newWeights
