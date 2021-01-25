76164ed4fec8da8b76f4b4493144d64e4c1bcd59
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-01-24 19:07:53 -0500

    Update main.py

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -9,16 +9,16 @@ import Debounce as D
 
 random.seed(1)
 
-dx = 0.001 #a hyperparameter
-rate = 0.1 #a hyperparameter
-beta = 0.9 #a hyperparameter
-theta = 0.55 #a hyperparameter for Debounce
-
 iters = 50
 optIter = 10
 inputs = [1, 0, 1, 0]
 outputs = [1, 1, 0, 1]
 weights = [[random.uniform(0, 1) for row in range(len(inputs)+1)] for row in range(len(inputs))]
+
+dx = 0.001
+rate = 0.1
+beta = 0.9
+theta = 0.55
 
 for row in range(iters):
 	weights = GD.optimize(inputs, weights, outputs, optIter, rate)
@@ -30,3 +30,9 @@ for row in range(iters):
 	print('Cost:', nn.cost(inputs, outputs, weights), '\n')
 
 print('Final Cost:', nn.cost(inputs, outputs, weights))
+
+#Gradient Descent Final Cost: 0.0012331873647288651
+#Momentum Final Cost: 0.00230511868475897
+#Nesterov Final Cost: 0.00230452251251315
+#NADAM Final Cost: 0.0021185030798304937
+#Debounce Final Cost: 0.0052760176928349336
