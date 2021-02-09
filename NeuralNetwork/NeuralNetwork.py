463c0aebdb9e79cd0052fa2ddd1a9a4f0c33de29
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-02-09 18:20:13 -0500

    Update NeuralNetwork.py

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,33 +1,30 @@
-import random
-import NeuralNetwork as nn
-import GradientDescent as GD
-import Momentum as M
-import Nesterov as NE
-import NADAM as NA
-import Debounce as D
+import numpy as np
 from copy import deepcopy
 
-random.seed(1)
+def generateWeights(layerData, inputQuantity):
+	layerDepthLimit = len(layerData)
+	augmentedLayerData = [inputQuantity] + deepcopy(layerData)
+	return [np.random.rand(layerData[layerDepth], augmentedLayerData[layerDepth] + 1)/2 for layerDepth in range(layerDepthLimit)]
 
-dx = 0.001 #a hyperparameter
-rate = 0.1 #a hyperparameter
-beta = 0.9 #a hyperparameter
-theta = 0.55 #a hyperparameter for Debounce
-tolerance = 0.0001 #the tolerance for the location of the vertex to count
-maxIter = 1000 #the maximum amount of iterations given to each model
+def layer(inputs, weights):
+	biasedInputs = np.append(np.array(inputs), 1)
+	neuronInputs = np.repeat(np.array([biasedInputs]), len(weights), axis = 0)
+	weightedInputs = neuronInputs * np.array(weights)
+	output = np.tanh(np.sum(weightedInputs, axis = 1))
+	return output
 
-iterations = 100
-inputs = [1, 0, 1, 0]
-outputs = [1, 1, 0, 1]
-weights = [[random.uniform(0, 1) for row in range(len(inputs)+1)] for row in range(len(inputs))]
+def neuralNetwork(inputs, weights):
+	outputs = []
+	layerInputs = deepcopy(inputs)
+	for layerWeights in weights:
+		layerInputs = layer(layerInputs, layerWeights)
+		outputs.append(deepcopy(layerInputs))
+	return outputs
 
-weights = GD.optimize(inputs, weights, outputs, iterations, rate)
-#weights = M.optimize(inputs, weights, outputs, iterations, rate, beta)
-#weights = NE.optimize(inputs, weights, outputs, iterations, rate, beta)
-#weights = NA.optimize(inputs, weights, outputs, iterations, rate, beta)
-#weights = D.optimize(inputs, weights, outputs, iterations, rate, beta, theta)
-
-inputs, outputs = nn.neuralNetwork(inputs, weights)
-
-for row in outputs:
-	print(sum(row)/len(row))
+def layerCost(inputs, weights, outputs):
+	neuronErrors = (outputs - layer(inputs, weights)) ** 2
+	return np.sum(neuronErrors)
+		
+def neuralNetworkCost(inputs, weights, outputs):
+	neuronErrors = (outputs - neuralNetwork(inputs, weights)[-1]) ** 2
+	return np.sum(neuronErrors)
