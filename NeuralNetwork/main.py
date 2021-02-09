46a830befebde68bd7c3860f40f13c1dd92d00a9
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-02-09 18:19:32 -0500

    Update main.py

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -1,41 +1,84 @@
 from copy import deepcopy
-import random
-import NeuralNetwork as nn
-import GradientDescent as GD
+import random; random.seed(1)
+import numpy as np
+import NeuralNetwork2 as nn
+import GradientDescent2 as GD
 import Momentum as M
 import Nesterov as NE
 import NADAM as NA
 import Debounce as D
+print('Imports Sucessfull')
 
-random.seed(1)
+def grab(path):
+	trainingDataFile = open(path, 'r')
+	trainingData = trainingDataFile.read()
+	trainingData = trainingData.split("\n")
+	trainingData = trainingData[1:len(trainingData) - 32000]
 
-iters = 52
-optIter = 1
-inputs = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-outputs = [1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0]
-weights = [[random.uniform(0, 1) for row in range(len(inputs)+1)] for row in range(len(inputs))]
+	for j in range(len(trainingData)):
+		trainingData[j] = trainingData[j].split(",")
+	for x in range(len(trainingData)):
+		for y in range(len(trainingData[x])):
+			trainingData[x][y] = int(trainingData[x][y])
+	trainingData = np.array(trainingData)
+
+	trainingInputs = trainingData.transpose()[1:len(trainingData) + 1].transpose()
+	trainingInputs = np.array([np.array(i) for i in trainingInputs])
+
+	trainingOutputs = trainingData.transpose()[0]
+	trainingOutputs = list(trainingOutputs)
+	for k in range(len(trainingOutputs)):
+		tmp = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+		tmp[trainingOutputs[k]] = 1
+		trainingOutputs[k] = deepcopy(tmp)
+	trainingOutputs = np.array(trainingOutputs)
+
+	return trainingInputs, trainingOutputs
+
+path = str(input('Path: '))
+dataPath = f'{path}\\train.csv'
+
+inputsD, outputsD = grab(dataPath)
+print('Data Formatted')
+
+iters = 1
 dx = 0.001
-rate = 1
+rate = 0.1
 beta = 0.9
-theta = 10000
+theta = 1000
 scale = 0.1
+layerData = [1, 16, 16, len(outputsD[0])]
 
-path = 'directory'
+weights = nn.generateWeights(layerData, len(inputsD[0]))
+
+print('Weights Initialized')
 
 open(f'{path}scores.txt', 'r+').truncate(0)
-
 for row in range(iters):
-	weights = GD.optimize(inputs, weights, outputs, optIter, rate)
-	#weights = M.optimize(inputs, weights, outputs, optIter, rate, beta)
-	#weights = NE.optimize(inputs, weights, outputs, optIter, rate, beta)
-	#weights = NA.optimize(inputs, weights, outputs, optIter, rate, beta)
-	#weights = D.optimize(inputs, weights, outputs, optIter, rate, beta, theta, scale)
-	
-	cost = nn.cost(inputs, outputs, weights)
-	print('Iteration', row+1)
-	print('Cost:', cost, '\n')
+	print('\nIteration:', row+1, '\n\n')
+	for inp in range(len(outputsD)):
+		inputs = inputsD[inp]
+		outputs = outputsD[inp]
+		
+		# netCorrect = GD.Network(inputs, weights, outputs, dx)
+		# weights = netCorrect.adjustNetwork(rate)
 
-	f = open(f'{path}scores.txt', 'a')
-	f.write(f'\n{cost}'); f.close()
+		weights = GD.backPropagation(inputs, weights, outputs, dx, rate)
+		#weights = M.optimize(inputs, weights, outputs, rate, dx, beta)
+		#weights = NE.optimize(inputs, weights, outputs, rate, dx, beta)
+		#weights = NA.optimize(inputs, weights, outputs, rate, dx, beta)
+		#weights = D.optimize(inputs, weights, outputs, rate, dx, beta, scale)
+		
+		cost = nn.neuralNetworkCost(inputs, weights, outputs)
+		
+		print('\nExample:', inp+1)
+		print('Cost:', cost)
 
-print('Final Cost:', cost)
+		f = open(f'{path}scores.txt', 'a')
+		f.write(f'\n{cost}'); f.close()
+
+		open(f'{path}weights.txt', 'r+').truncate(0)
+		f = open(f'{path}weights.txt', 'a')
+		f.write(f'{list(weights)}'); f.close()
+
+	print('Final Cost:', cost)
