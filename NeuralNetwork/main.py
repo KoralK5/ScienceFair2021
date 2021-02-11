91ffbf158233f34cf8e42e029e039759a88bc16a
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-02-11 11:27:23 -0500

    Update main.py

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
+import time
 import random; random.seed(1)
 import numpy as np
-import NeuralNetwork2 as nn
-import GradientDescent2 as GD
+import NeuralNetwork as nn
+import GradientDescent as GD
 import Momentum as M
 import Nesterov as NE
 import NADAM as NA
@@ -13,7 +14,7 @@ def grab(path):
 	trainingDataFile = open(path, 'r')
 	trainingData = trainingDataFile.read()
 	trainingData = trainingData.split("\n")
-	trainingData = trainingData[1:len(trainingData) - 32000]
+	trainingData = trainingData[1:len(trainingData)-1]
 
 	for j in range(len(trainingData)):
 		trainingData[j] = trainingData[j].split(",")
@@ -35,13 +36,12 @@ def grab(path):
 
 	return trainingInputs, trainingOutputs
 
-path = str(input('Path: '))
+path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\ScienceFair21\\NeuralNetwork\\'
 dataPath = f'{path}\\train.csv'
 
 inputsD, outputsD = grab(dataPath)
 print('Data Formatted')
 
-iters = 1
 dx = 0.001
 rate = 0.1
 beta = 0.9
@@ -52,33 +52,43 @@ layerData = [1, 16, 16, len(outputsD[0])]
 weights = nn.generateWeights(layerData, len(inputsD[0]))
 
 print('Weights Initialized')
+print('Training...')
 
 open(f'{path}scores.txt', 'r+').truncate(0)
-for row in range(iters):
-	print('\nIteration:', row+1, '\n\n')
+start = time.time()
+
+num, cost = 0, 0
+while time.time()-start < 43200:
 	for inp in range(len(outputsD)):
 		inputs = inputsD[inp]
 		outputs = outputsD[inp]
-		
+
 		# netCorrect = GD.Network(inputs, weights, outputs, dx)
 		# weights = netCorrect.adjustNetwork(rate)
 
-		weights = GD.backPropagation(inputs, weights, outputs, dx, rate)
+		weights, newOutputs = GD.backPropagation(inputs, weights, outputs, dx, rate)
 		#weights = M.optimize(inputs, weights, outputs, rate, dx, beta)
 		#weights = NE.optimize(inputs, weights, outputs, rate, dx, beta)
 		#weights = NA.optimize(inputs, weights, outputs, rate, dx, beta)
 		#weights = D.optimize(inputs, weights, outputs, rate, dx, beta, scale)
 		
-		cost = nn.neuralNetworkCost(inputs, weights, outputs)
+		cost += nn.neuralNetworkCost(inputs, weights, outputs)
 		
-		print('\nExample:', inp+1)
-		print('Cost:', cost)
+		if not (inp+1)%100:
+			print('\n\nNetwork:', num+1)
+			print(f'Time: {int(43200-time.time()+start)}s')
+			print('Cost:', cost/100)
+			print('\nPred:', newOutputs)
+			print('Real:', outputs)
 
-		f = open(f'{path}scores.txt', 'a')
-		f.write(f'\n{cost}'); f.close()
+			f = open(f'{path}scores.txt', 'a')
+			f.write(f'\n{cost/100}'); f.close()
+			cost = 0
 
 		open(f'{path}weights.txt', 'r+').truncate(0)
 		f = open(f'{path}weights.txt', 'a')
-		f.write(f'{list(weights)}'); f.close()
+		f.write(f'{weights}'); f.close()
+		num += 1
 
-	print('Final Cost:', cost)
+print('Final Cost:', cost)
+print('Iterations:', num)
