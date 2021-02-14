45ef94e88a4b3a22a37fb8a8e387a94af1fe96a7
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-02-14 14:44:43 -0500

    Update testing.py

--- NeuralNetwork/testing.py
+++ NeuralNetwork/testing.py
@@ -1,9 +1,7 @@
 from copy import deepcopy
-from ast import literal_eval
+from PIL import Image
 import numpy as np
 import NeuralNetwork as nn
-
-path = 'path\\'
 
 def grab(path):
 	trainingData = open(path, 'r+').read().split("\n")
@@ -29,26 +27,36 @@ def getWeights(path):
 		path = np.load(f, allow_pickle=True)
 	return path
 
+path = 'path\\'
+
 inputs, outputs = grab(f'{path}test.csv')
-weights = getWeights(f'{path}weights.npy')
+weightsGD = getWeights(f'{path}GDweights.npy')
+weightsM = getWeights(f'{path}Mweights.npy')
+weightsNE = getWeights(f'{path}NEweights.npy')
+weightsNA = getWeights(f'{path}NAweights.npy')
+weightsD = getWeights(f'{path}Dweights.npy')
 
-dx = 0.001
-rate = 0.1
-beta = 0.9
-scale = 0.1
 num = 0
-
 for row in range(len(outputs)):
-	newOutputs = nn.neuralNetwork(inputs[row], weights)[-1]
+	newOutputsGD = nn.neuralNetwork(inputs[row], weightsGD)[-1]
+	newOutputsM = nn.neuralNetwork(inputs[row], weightsM)[-1]
+	newOutputsNE = nn.neuralNetwork(inputs[row], weightsNE)[-1]
+	newOutputsNA = nn.neuralNetwork(inputs[row], weightsNA)[-1]
+	newOutputsD = nn.neuralNetwork(inputs[row], weightsD)[-1]
 
 	real = list(outputs[row]).index([1])
-	pred = list(newOutputs).index(max(list(newOutputs)))
-	input('- ')
-	print('Real:', real)
-	print('\nPred:', pred)
-	print(newOutputs)
+	predGD = list(newOutputsGD).index(max(list(newOutputsGD)))
+	predM = list(newOutputsM).index(max(list(newOutputsM)))
+	predNE = list(newOutputsNE).index(max(list(newOutputsNE)))
+	predNA = list(newOutputsNA).index(max(list(newOutputsNA)))
+	predD = list(newOutputsD).index(max(list(newOutputsD)))
 
-	if pred == real:
-		num += 1
+	input('\n- ')
+	print(f'Real: {real}\n')
 
-print(num/row)
+	print('      Guess | Certainty')
+	print(f'GD:       {predGD} | {int(max(list(newOutputsGD))*100)}%')
+	print(f'M :       {predM} | {int(max(list(newOutputsM))*100)}%')
+	print(f'NE:       {predNE} | {int(max(list(newOutputsNE))*100)}%')
+	print(f'NA:       {predNA} | {int(max(list(newOutputsNA))*100)}%')
+	print(f'D :       {predD} | {int(max(list(newOutputsD))*100)}%')
