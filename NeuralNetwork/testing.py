2cd600eb981bb6e89866a1f7e0e21b8a814114c3
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-02-14 01:28:42 -0500

    Update testing.py

--- NeuralNetwork/testing.py
+++ NeuralNetwork/testing.py
@@ -3,21 +3,15 @@ from ast import literal_eval
 import numpy as np
 import NeuralNetwork as nn
 
-path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\ScienceFair21\\NeuralNetwork\\'
+path = 'path\\'
 
 def grab(path):
-	trainingData = open(path, 'r').read()
-	trainingData = trainingData.split("\n")
+	trainingData = open(path, 'r+').read().split("\n")
 	trainingData = trainingData[1:len(trainingData)-1]
 
-	for j in range(len(trainingData)):
-		trainingData[j] = trainingData[j].split(",")
-	for x in range(len(trainingData)):
-		for y in range(len(trainingData[x])):
-			trainingData[x][y] = int(trainingData[x][y])
-	trainingData = np.array(trainingData)
+	trainingData = np.array([[int(col) for col in row.split(',')] for row in trainingData])
 
-	trainingInputs = trainingData.transpose()[1:len(trainingData) + 1].transpose()
+	trainingInputs = [row/255 for row in trainingData.transpose()[1:len(trainingData) + 1].transpose()]
 	trainingInputs = np.array([np.array(i) for i in trainingInputs])
 
 	trainingOutputs = trainingData.transpose()[0]
@@ -29,7 +23,7 @@ def grab(path):
 	trainingOutputs = np.array(trainingOutputs)
 
 	return trainingInputs, trainingOutputs
-	
+
 def getWeights(path):
 	with open(path, 'rb') as f:
 		path = np.load(f, allow_pickle=True)
@@ -42,18 +36,19 @@ dx = 0.001
 rate = 0.1
 beta = 0.9
 scale = 0.1
-
 num = 0
+
 for row in range(len(outputs)):
 	newOutputs = nn.neuralNetwork(inputs[row], weights)[-1]
 
 	real = list(outputs[row]).index([1])
 	pred = list(newOutputs).index(max(list(newOutputs)))
+	input('- ')
+	print('Real:', real)
+	print('\nPred:', pred)
+	print(newOutputs)
 
-	#print('Real:', real)
-	#print('\nPred:', pred)
-
-	if real == pred:
+	if pred == real:
 		num += 1
 
 print(num/row)
