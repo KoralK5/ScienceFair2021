7fad43528be73fe4b3c0fa91750c89cf7796ac66
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-03-06 11:17:49 -0500

    Update snakeMain.py

--- NeuralNetwork/Games/Snake/snakeMain.py
+++ NeuralNetwork/Games/Snake/snakeMain.py
@@ -8,13 +8,13 @@ import snakeNN as nn
 import snakeNNgame as game
 print('Imports Sucessfull')
 
-path = 'path\\'
+path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\ScienceFair21\\NeuralNetwork\\Games\\Snake\\snakeData\\'
 dataPath = f'{path}\\train.csv'
 
 changeAmount = 500000
 mutationAmount = 100
 decimal = 100
-batch = 100
+batch = 5
 layerData = [80, 40, 20, 4]
 
 weights = nn.generateWeights(layerData, 10*10+1)
@@ -28,6 +28,9 @@ start, inp, speed, disp = time.time(), 0, 0, 0
 while True:
 	changeAmount -= int(changeAmount*0.1) + 1000
 	inp += 1
+
+	if batch < 100:
+		batch += 1
 
 	weightsBatch = [[]]
 	costBatch = [0]
