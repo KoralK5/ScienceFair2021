b21a989a7967843c28ced9c3db1243bfc9f25b39
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-03-06 01:26:37 -0500

    Update snakeMain.py

--- NeuralNetwork/Games/Snake/snakeMain.py
+++ NeuralNetwork/Games/Snake/snakeMain.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
 import time
-import random; random.seed(1)
+import random
 import numpy as np
 import os
+import keyboard
 import snakeNN as nn
 import snakeNNgame as game
 print('Imports Sucessfull')
@@ -10,41 +11,46 @@ print('Imports Sucessfull')
 path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\ScienceFair21\\NeuralNetwork\\Games\\Snake\\snakeData\\'
 dataPath = f'{path}\\train.csv'
 
-changeAmount = 50
-mutationAmount = 50
+changeAmount = 500000
+mutationAmount = 100
 decimal = 100
-batch = 25
-layerData = [16, 16, 4]
+batch = 100
+layerData = [80, 40, 20, 4]
 
-weights = nn.generateWeights(layerData, 10*10)
+weights = nn.generateWeights(layerData, 10*10+1)
 
 print('Weights Initialized')
 print('Training...')
 
 open(f'{path}snakeScores.csv', 'r+').truncate(0)
 
-start, inp = time.time(), 0
+start, inp, speed, disp = time.time(), 0, 0, 0
 while True:
+	changeAmount -= int(changeAmount*0.1) + 1000
 	inp += 1
-	weightsBatch, costBatch = [], []
+
+	weightsBatch = [[]]
+	costBatch = [0]
+	
 	for net in range(batch):
-		netWeights = deepcopy(weights)
-		netWeights = nn.mutate(netWeights, changeAmount, mutationAmount, decimal)
-
-		costBatch.append(game.play(netWeights))
-		weightsBatch.append(netWeights)
-		
+		netWeights = nn.mutate(weights, changeAmount, mutationAmount, decimal)
 		t = int(time.time() - start)
-		print('\n\nGeneration:', inp)
-		print('Network:', net)
-		print(f'Time: {t}s')
-		print('Lifespan:', costBatch[-1])
-		print('Best:', max(costBatch))
-		time.sleep(1)
-		os.system('cls')
+		
+		if keyboard.is_pressed('p'):
+			disp = abs(disp-1)
 
+		costBatch.append(game.play(netWeights, inp, net, t, costBatch, disp))
+		weightsBatch.append(netWeights)
+
+	os.system('cls')
+	
 	f = open(f'{path}snakeScores.csv', 'a')
 	f.write(f'\n{max(costBatch)}'); f.close()
 
 	weights = weightsBatch[costBatch.index(max(costBatch))]
 	np.save(f'{path}genWeights.npy', np.array(weights, dtype=object))
+
+	print('Generation:', inp)
+	print('Network:', inp*(net+1))
+	print(f'Time: {t}s')
+	print('Best:', max(costBatch))
